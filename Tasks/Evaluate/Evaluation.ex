0a88e791a1a76763667db8387f547f066c90aeed
Alexander Lundqvist <33635814+KrimReaper@users.noreply.github.com>
2023-01-30 20:55:02 +0100

    Update Evaluation.ex

--- Tasks/Evaluate/Evaluation.ex
+++ Tasks/Evaluate/Evaluation.ex
@@ -1,26 +1,40 @@
 defmodule Evaluation do
 
-  @type expr() :: literal()
-  | {:add, expr(), expr()}
-  | {:sub, expr(), expr()}
-  | {:mul, expr(), expr()}
-  | {:div, expr(), expr()}
+  @type literal() :: {:num, number()} | {:var, atom()} | | {:q, number(), number()}
+
+  @type expression() :: literal()
+  | {:add, expression(), expression()}
+  | {:sub, expression(), expression()}
+  | {:mul, expression(), expression()}
+  | {:div, expression(), expression()}
 
 
-  def eval({:num, _number}, _number) do number end
+  def eval({:num, number}, number) do {:num, number} end
 
-  def eval({:var, x}, x) do x end
+  def eval({:var, x}, x) do {:var, x} end
 
-  def eval({:add, ..., ...}, ...) do
-    add(..., ...)
+  def eval({:add, expression1, expression2}, x) do
+    add({expression1}, {})
   end
 
-  def eval({:add, ..., ...}, ...) do
-    add(..., ...)
+  def eval({:sub, ..., ...}, ...) do
+    sub(..., ...)
   end
 
-  def eval({:add, ..., ...}, ...) do
-    add(..., ...)
+  def eval({:mul, ..., ...}, ...) do
+    mul(..., ...)
+  end
+
+  def eval({:div, ..., ...}, ...) do
+    div(..., ...)
+  end
+
+  def add({:num, number1}, {:num, number2}) do
+    number1 + number2
+  end
+
+  def add({:num, number1}, {:num, number2}) do
+    number1 + number2
   end
 
 end
