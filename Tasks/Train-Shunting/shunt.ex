2a80c674568c36450b04ce8f2b466910244cd404
Alexander Lundqvist <Alexander.lundqvist@outlook.com>
2023-03-06 16:04:52 +0100

    Wip, moving to laptop

--- Tasks/Train-Shunting/shunt.ex
+++ Tasks/Train-Shunting/shunt.ex
@@ -2,4 +2,80 @@ defmodule Shunt do
   @moduledoc """
   TBD
   """
+
+
+  def find([], []) do [] end
+  def find(xs, [y|ys]) do
+    {hs, ts} = Train.split(xs, y)
+    tn = length(ts)
+    hn = length(hs)
+    [
+      {:one, tn+1},
+      {:two, hn},
+      {:one, -(tn+1)},
+      {:two, -hn} | find(Train.append(hs, ts), ys)
+    ]
+  end
+
+  @doc """
+
+  """
+  def few([], []) do
+    []
+  end
+
+
+  def few(xs, [y | ys]) do
+    {hs, ts} = Train.split(xs, y)
+    n_hs = length(hs)
+    n_ts = length(ts)
+
+    moves = [
+      {:one, n_ts + 1},
+      {:two, n_hs},
+      {:one, -(n_ts + 1)},
+      {:two, -n_hs}
+    ]
+
+    if n_hs == 0 do
+      [] ++ few(ts, ys)
+    else
+      moves ++ few(ts ++ hs, ys)
+    end
+  end
+
+
+  def compress(ms) do
+    ns = rules(ms)
+    if ns == ms do
+      ms
+    else
+      compress(ns)
+    end
+  end
+
+  def rules([]) do
+    []
+  end
+
+  def rules([{track, step} | moves]) do
+    cond do
+      step == 0 ->
+        rules(moves)
+
+      moves != [] ->
+        [{nextTrack, nextStep} | nextMoves] = moves
+        cond do
+          track == :one && nextTrack == :one ->
+            [{:one, step + nextStep}] ++ rules(nextMoves)
+          track == :two && nextTrack == :two ->
+            [{:two, step + nextStep}] ++ rules(nextMoves)
+          true ->
+            [{track, step}] ++ rules(moves)
+        end
+      true ->
+        [{track, step}] ++ rules(moves)
+    end
+  end
+
 end
--- Tasks/Train-Shunting/train.ex
+++ Tasks/Train-Shunting/train.ex
@@ -1,86 +1,41 @@
 defmodule Train do
   @moduledoc """
-  This module represents our trains and it defines operatations we can perform on the train
-  structure that the train is represented by.
+  This module represents trains as lists of wagons and it defines operatations we can
+  perform on said structures.
   """
 
   @doc """
-  Returns the first n wagons of a train if n <= the lenght of the train, otherwise
-  return the entire train.
   """
   def take(_, 0) do [] end
+  def take([head|tail], n) when n > 0 do [head|take(tail, n-1)] end
 
   @doc """
-  Returns the first n wagons of a train if n <= 0, otherwise return the .
-  If n is greater than 0, returns a train containing the first n wagons.
+
   """
-  def take([h|t], n) when n > 0 do [h|take(t, n-1)] end
+  def drop(list, 0) do lst end
+  def drop([_|tail], n) when n > 0 do drop(tail, n-1) end
 
   @doc """
-  Detatches the first n wagons of a train.
-  If n is zero, returns the entire train.
+
   """
-  def drop(lst, 0) do lst end
+  def append([], secondTrain) do secondTrain end
+  def append([head|tail], secondTrain) do [head|append(tail, secondTrain)] end
 
   @doc """
-  Detatches the first n wagons of a train.
 
-  If n is greater than 0, drops the first n wagons and returns the rest of the train.
   """
-  def drop([_|t], n) when n > 0 do drop(t, n-1) end
-
-  @doc """
-  Appends two trains.
-
-  If the first train is empty, returns the second train.
-  """
-  def append([], ys) do ys end
-
-  @doc """
-  Appends two trains.
-
-  If the first train is not empty, appends the first train and the second train.
-  """
-  def append([h|t], ys) do [h|append(t,ys)] end
-
-  @doc """
-  Checks if an element is a member of a train.
-
-  If the train is empty, returns false.
-  """
-  def member([],_) do false end
-
-  @doc """
-  Checks if an element is a member of a train.
-
-  If the element is the first element of the train, returns true.
-  """
+  def member([], _) do false end
   def member([y|_], y) do true end
+  def member([_|tail], y) do member(tail, y) end
 
   @doc """
-  Checks if an element is a member of a train.
 
-  If the element is not the first element of the train, checks the rest of the train.
-  """
-  def member([_|t], y) do member(t, y) end
-
-  @doc """
-  Returns the position of an element in a train.
-
-  If the element is the first element of the train, returns 1.
   """
   def position([y|_], y) do 1 end
+  def position([_|tail], y) do position(tail, y) + 1 end
 
   @doc """
-  Returns the position of an element in a train.
 
-  If the element is not the first element of the train, adds 1 to the position of the element in the rest of the train.
-  """
-  def position([_|t], y) do position(t, y) + 1 end
-
-  @doc """
-  Splits a train into two trains at the position of an element.
-  Returns a tuple containing the first part of the train before the element and the second part of the train after the element.
   """
   def split(train, y) do
     n = position(train, y)
