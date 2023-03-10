e4d400d349b16f2c565fa9fdcc0b1e18c7909f87
Alexander Lundqvist <Alexander.lundqvist@outlook.com>
2023-03-10 17:34:52 +0100

    Train shunt done

--- Tasks/Train-Shunting/moves.ex
+++ Tasks/Train-Shunting/moves.ex
@@ -1,40 +1,82 @@
 defmodule Moves do
   @moduledoc """
-  TBD
+  This module represents the moves we can perform on the trains at the train shunting
+  yard, represented as a tuple of three lists:
+    - the main track
+    - track_1
+    - track_2
+  Moves are defined as a tuple of an atom (:track_1 or :track_2) and an integer n,
+  where n represents the number of wagons to move and the sign represents in which
+  direction the move is. Positive values of n represent a move to the right (from the main track to
+  the specified shunting track), while negative values of n represent a move
+  to the left (from the specified shunting track to the main track).
   """
 
   @doc """
+  Executes a single move on a train shunting yard state. It takes a move and an
+  input state (where the state is a list of three lists representing the train tracks)
+  and return a new state after the move has been performed.
 
+  ## Examples
+
+      iex> Moves.single({:track_1, 2}, {[1,2,3,4], [5,6,7], [8,9,10]})
+      {[3,4], [5,6,7,1,2], [8,9,10]}
+
+      iex> Moves.single({:track_2, -1}, {[1,2,3], [4,5,6], [7,8,9,10]})
+      {[1,2,3,6], [4,5], [7,8,9,10]}
+  """
+  # If we move zero wagons, then nothing has changed and we return the same state.
+  def single({_, 0}, state) do state end
+
+  # If n is positive, move n wagons from main track to track_1
+  def single({:track_1, n}, {main, track_1, track_2}) when n > 0 do
+    {0, remain, wagons} = Train.main(main, n)
+    {remain, Train.append(wagons, track_1), track_2}
+  end
+
+  # If n is negative, move |n| wagons from track_1 to main track
+  def single({:track_1, n}, {main, track_1, track_2}) when n < 0 do
+    wagons = Train.take(track_1, -n)
+    {Train.append(wagons, main), Train.drop(track_1, -n), track_2}
+  end
+
+  # If n is positive, move n wagons from main track to track_2
+  def single({:track_2, n}, {main, track_1, track_2}) when n > 0 do
+    {0, remain, wagons} = Train.main(main, n)
+    {remain, track_1, Train.append(wagons, track_2)}
+  end
+
+  # If n is negative, move |n| wagons from track_2 to main track
+  def single({:track_2, n}, {main, track_1, track_2}) when n < 0 do
+    wagons = Train.take(track_2, -n)
+    {Train.append(wagons, main), track_1, Train.drop(track_2, -n)}
+  end
+
+
+  @doc """
+  Executes a sequence of moves on a train shunting yard state. It takes
+  a list of moves and a track state and returns a list of states representing
+  the intermediate states after each move is applied.
+
+  ## Example from the assignment instructions
+  iex> Moves.sequence([{:track_1,1},{:track_2,2},{:track_1, -1},{:track_2,-2},{:track_1,1},
+                       {:track_2,1}, {:track_1,-1},{:track_2,-1}],{[:a,:b,:c],[],[]})
+
+    [
+      {[:a, :b, :c], [], []},
+      {[:a, :b], [:c], []},
+      {[], [:c], [:a, :b]},
+      {[:c], [], [:a, :b]},
+      {[:c, :a, :b], [], []},
+      {[:c, :a], [:b], []},
+      {[:c], [:b], [:a]},
+      {[:c, :b], [], [:a]},
+      {[:c, :b, :a], [], []}
+    ]
   """
   def sequence([], state) do [state] end
   def sequence([move|rest], state) do
     [state | sequence(rest, single(move, state))]
   end
-
-  @doc """
-
-  """
-  def single({_, 0}, state)  do state end
-
-  def single({:one, n}, {main, one, two}) when n > 0 do
-    wgns = Train.take(main, n)
-    {Train.drop(main, n), Train.append(wgns, one), two}
-  end
-
-  def single({:one, n}, {main, one, two}) when n < 0 do
-    wgns = Train.take(one, -n)
-    {Train.append(wgns, main), Train.drop(one, -n), two}
-  end
-
-  def single({:two, n}, {main, one, two}) when n > 0 do
-    wgns = Train.take(main, n)
-    {Train.drop(main, n), one, Train.append(wgns, two)}
-  end
-
-  def single({:two, n}, {main, one, two}) when n < 0 do
-    wgns = Train.take(two, -n)
-    {Train.append(wgns, main), one, Train.drop(two, -n)}
-  end
-
 
 end
--- Tasks/Train-Shunting/train.ex
+++ Tasks/Train-Shunting/train.ex
@@ -1,45 +1,72 @@
 defmodule Train do
   @moduledoc """
   This module represents trains as lists of wagons and it defines operatations we can
-  perform on said structures.
+  perform on said structures. Wagons are represented as atoms (:a, :b, :c, ...).
+  Each wagon in a train must be unique, i.e there can be no duplicates so [:a,:b]
+  is a valid configuration but [:a,:a] is not.
   """
 
   @doc """
+  Takes the first n wagons of the train and returns them as a new train.
+  If n is greater than the length of the train, it returns the entire train.
   """
   def take(_, 0) do [] end
   def take([head|tail], n) when n > 0 do [head|take(tail, n-1)] end
 
   @doc """
-
+  Drops the first n wagons of the train and returns the remaining wagons as a new train.
+  If n is greater than the length of the train, it returns an empty list.
   """
-  def drop(list, 0) do lst end
+  def drop(train, 0) do train end
   def drop([_|tail], n) when n > 0 do drop(tail, n-1) end
 
   @doc """
-
+  Appends the wagons of the train_1 (denoted by [head|tail]) to the end of train_2 and returns the resulting train.
   """
-  def append([], secondTrain) do secondTrain end
-  def append([head|tail], secondTrain) do [head|append(tail, secondTrain)] end
+  def append([], train_2) do train_2 end
+  def append([head|tail], train_2) do [head|append(tail, train_2)] end
 
   @doc """
-
+  Checks if the wagon is a member of the train. It returns true if wagon is in the train, and false otherwise.
   """
   def member([], _) do false end
-  def member([y|_], y) do true end
-  def member([_|tail], y) do member(tail, y) end
+  def member([wagon|_], wagon) do true end
+  def member([_|tail], wagon) do member(tail, wagon) end
 
   @doc """
-
+  Returns the position of the first occurrence of the wagon in the train. The positions are 1-indexed.
   """
-  def position([y|_], y) do 1 end
-  def position([_|tail], y) do position(tail, y) + 1 end
+  def position([wagon|_], wagon) do 1 end
+  def position([_|tail], wagon) do position(tail, wagon) + 1 end
 
   @doc """
-
+  Returns a tuple with two trains where the first contains all the wagons before the specified wagon and the
+  second contains all the wagons after the specified wagon.
   """
-  def split(train, y) do
-    n = position(train, y)
-    {take(train, n-1), drop(train, n)}
+  def split(train, wagon) do
+    index = position(train, wagon)
+    {take(train, index-1), drop(train, index)}
+  end
+
+  @doc """
+  This function is used when moving trains FROM the main track. It takes
+  two arguments: a train (i.e. a list of wagons) and a number n that represents
+  the number of wagons to move. The wagons on the main track are in reverse order
+  i.e. the first wagon in the list is in the leftmost position on the track.
+  When you’re asked to move two wagons to another track you should divide the train
+  into two segments; the segment that should remain and the two wagons
+  (to the right i.e. in the end of the list) that should be moved.
+
+  For example:
+    main([:a, :b, :c, :d], 3) = {0, [:a], [:b, :c, .d]}
+    main([:a, :b, :c, :d], 5) = {1, [], [:a, :b, :c, .d]}
+  """
+  def main([], n) do {n, [], []} end
+  def main([head|tail], n) do
+    case main(tail, n) do
+      {0, remain, take} -> {0, [head|remain], take}
+      {n, remain, take} -> {n-1, remain, [head|take]}
+    end
   end
 
 end
