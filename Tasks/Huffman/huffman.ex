4472873e6dd57e0c83e3df8e6fcda17c230994e1
Alexander Lundqvist <Alexander.lundqvist@outlook.com>
2023-03-18 12:47:42 +0100

    Switch to laptop

--- Tasks/Huffman/huffman.ex
+++ Tasks/Huffman/huffman.ex
@@ -1,12 +1,6 @@
 defmodule Huffman do
-  @moduledoc """
-  TBD
-  """
 
-  @doc """
-  TBD
-  """
-  def sample do
+  def sample() do
     'the quick brown fox jumps over the lazy dog
     this is a sample text that we will use when we build
     up a table we will only handle lower case letters and
@@ -14,16 +8,11 @@ defmodule Huffman do
     represent english but it is probably not that far off'
   end
 
-  @doc """
-  TBD
-  """
   def text() do
     'this is something that we should encode'
   end
 
-  @doc """
-  TBD
-  """
+
   def test do
     sample = sample()
     tree = tree(sample)
@@ -34,39 +23,131 @@ defmodule Huffman do
     decode(seq, decode)
   end
 
-  @doc """
-  TBD
-  """
+
   def tree(sample) do
-  # To implement...
+    freq = freq(sample)
+    huffman(freq)
   end
 
-  @doc """
-  TBD
-  """
+  def freq(sample) do
+    freq(sample, [])
+  end
+  def freq([], freq) do
+    freq
+  end
+  def freq([char | rest], freq) do
+    freq(rest, update(char, freq))
+  end
+
+
+  defp update(char, []) do
+    [{char, 1}]
+  end
+  defp update(char, [{char, n} | freq]) do
+    [{char, n + 1} | freq]
+  end
+  defp update(char, [elem | freq]) do
+    [elem | update(char, freq)]
+  end
+
+
+  def huffman(freq) do
+    sorted = Enum.sort(freq, fn {_, x}, {_, y} -> x < y end)
+    huffman_tree(sorted)
+  end
+  defp huffman_tree([{tree, _}]), do: tree
+  defp huffman_tree([{a, af}, {b, bf} | rest]) do
+    huffman_tree(insert({{a, b}, af + bf}, rest))
+  end
+  defp insert({a, af}, []), do: [{a, af}]
+  defp insert({a, af}, [{b, bf} | rest]) when af < bf do
+    [{a, af}, {b, bf} | rest]
+  end
+  defp insert({a, af}, [{b, bf} | rest]) do
+    [{b, bf} | insert({a, af}, rest)]
+  end
+
   def encode_table(tree) do
-  # To implement...
+    Enum.sort(tree,[])
   end
 
-  @doc """
-  TBD
-  """
-  def decode_table(tree) do
-  # To implement...
+  def codes({a, b}, current_position) do
+    as = codes(a, [0 | current_position])
+    bs = codes(b, [1 | current_position])
+    as ++ bs
+  end
+  def codes(a, code) do
+    [{a, Enum.reverse(code)}]
   end
 
-  @doc """
-  TBD
-  """
-  def encode(text, table) do
-  # To implement...
+  def encode([], _), do: []
+  def encode([char | rest], table) do
+    {_, code} = List.keyfind(table, char, 0)
+    code ++ encode(rest, table)
   end
 
-  @doc """
-  TBD
-  """
-  def decode(seq, tree) do
-  # To implement...
+  def decode_table(tree), do: codes(tree, [])
+
+  def decode([], _), do: []
+  def decode(seq, table) do
+    {char, rest} = decode_char(seq, 1, table)
+    [char | decode(rest, table)]
   end
 
+  def decode_char(seq, n, table) do
+    {code, rest} = Enum.split(seq, n)
+    case List.keyfind(table, code, 1) do
+      {char, _} ->
+        {char, rest}
+      nil ->
+        decode_char(seq, n + 1, table)
+    end
+  end
+
+
+  def bench(n) do
+    bench("./kallocain.txt", n)
+  end
+  def bench(file, n) do
+    {text, b} = read(file, n)
+    c = length(text)
+    {tree, t2} = time(fn -> tree(text) end)
+    {encode, t3} = time(fn -> encode_table(tree) end)
+    s = length(encode)
+    {decode, _} = time(fn -> decode_table(tree) end)
+    {encoded, t5} = time(fn -> encode(text, encode) end)
+    e = div(length(encoded), 8)
+    r = Float.round(e / b, 3)
+    {_, t6} = time(fn -> decode(encoded, decode) end)
+
+    IO.puts("text of #{c} characters")
+    IO.puts("tree built in #{t2} ms")
+    IO.puts("table of size #{s} in #{t3} ms")
+    IO.puts("encoded in #{t5} ms")
+    IO.puts("decoded in #{t6} ms")
+    IO.puts("source #{b} bytes, encoded #{e} bytes, compression #{r}")
+  end
+
+  # Measure the execution time of a function.
+  def time(func) do
+    initial = Time.utc_now()
+    result = func.()
+    final = Time.utc_now()
+    {result, Time.diff(final, initial, :microsecond) / 1000}
+  end
+
+ # Get a suitable chunk of text to encode.
+  def read(file, n) do
+   {:ok, fd} = File.open(file, [:read, :utf8])
+    binary = IO.read(fd, n)
+    File.close(fd)
+
+    length = byte_size(binary)
+    case :unicode.characters_to_list(binary, :utf8) do
+      {:incomplete, chars, rest} ->
+        {chars, length - byte_size(rest)}
+      chars ->
+        {chars, length}
+    end
+  end
 end
